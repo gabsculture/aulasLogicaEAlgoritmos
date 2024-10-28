ee4c390fe33a00845e3e08cd4ff8f1fa3542b1e5
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 19:25:25 -0300

    Update lista2exercicio5.c

--- lista2exercicio5.c
+++ lista2exercicio5.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int valor = 0;
+    int maiorValor = 0;
+    for(int i = 1; i <=3; i++) {
+        printf("Digite um numero: \n");
+        scanf("%d", &valor);
+        if(valor > maiorValor) {
+            maiorValor = valor;
+        }
+    }
+    printf("Maior numero: %d", maiorValor);
+}
