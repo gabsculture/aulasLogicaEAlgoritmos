3c438cad81d0333099e03f0962ce4fe15285a3c1
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 19:22:01 -0300

    Update lista2exercicio4.c

--- lista2exercicio4.c
+++ lista2exercicio4.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int id, horasTrabalhadas, valorPorHora;
+    printf("Digite seu numero de identificacao:\n");
+    scanf("%d", &id);
+
+    printf("Digite a quantidade de horas trabalhadas: \n");
+    scanf("%d", &horasTrabalhadas);
+
+    printf("Digite o valor recebido por hora: \n");
+    scanf("%d", &valorPorHora);
+
+    float salario = valorPorHora * horasTrabalhadas;
+
+    printf("Identificacao: %d\n", id);
+    printf("Salario: %.2f\n", salario);
+}
