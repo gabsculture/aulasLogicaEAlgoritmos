00260715d6632c3b0990de7d6dc185362ebaa2e3
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 19:32:47 -0300

    Update lista2exercicio7.c

--- lista2exercicio7.c
+++ lista2exercicio7.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int valor;
+    printf("Digite um numero: ");
+    scanf("%d", &valor);
+
+    if (valor >= 2) {
+        int cedulas = valor / 2;
+        printf("O valor pode ser dividido em %d cedulas de 2 reais", cedulas);
+    } else {
+        printf("O valor nao pode ser divido em cedulas, pois eh menor que o menor valor de cedula do real (2)");
+    }
+}
