5a64945120eb14108de934832a21d22140d018bf
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 19:17:20 -0300

    Update lista2exercicio3.c

--- lista2exercicio3.c
+++ lista2exercicio3.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int dias;
+
+    printf("Digite a quantidade de dias para converter:\n");
+    scanf("%d", &dias);
+
+    float ano = dias/365;
+    float semanas = dias/7;
+
+    printf("Em anos: %2.f\n", ano);
+    printf("Em semanas: %2.f", semanas);
+    printf("Em dias: %d", dias);
+}
