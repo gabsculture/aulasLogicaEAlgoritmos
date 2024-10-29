0a882c72134552033d587dfe62fa24f58da0bef2
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 22:00:08 -0300

    Update lista2exercicio8.c

--- lista2exercicio8.c
+++ lista2exercicio8.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int valor;
+    printf("Digite um numero: ");
+    scanf("%d", &valor);
+
+    int horas = valor/3600;
+    int minutos = valor%3600/60;
+    int segundos = valor%3600%60;
+
+    printf("O valor em horas: %d", horas);
+    printf("O valor em minutos: %d", minutos);
+    printf("O valor em segundos: %d", segundos);
+}
