840a23d7797c9af260387f34346275b12ea4087f
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 22:41:21 -0300

    Update lista2exercicio14.c

--- lista2exercicio14.c
+++ lista2exercicio14.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    char letra;
+    char vogais[] = {'a', 'e', 'i', 'o', 'u'};
+    printf("Escolha uma letra: ");
+    scanf("%c", &letra);
+
+    for (int i = 0; i < (sizeof(vogais) / sizeof(vogais[0])); i++) {
+        if(letra == vogais[i]) {
+            printf("Eh uma vogal");
+            break;
+        } else {
+            printf("Eh consoante");
+            break;
+        }
+    }
+}
