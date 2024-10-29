20e15af4a3d6b6b6031eb1b795d7dd7d7767614a
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 22:17:54 -0300

    Update lista2exercicio12.c

--- lista2exercicio12.c
+++ lista2exercicio12.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int valor;
+    int numerosPositivos = 0;
+    int soma = 0;
+    for (int i = 1; i <= 5; i++) {
+        printf("Digite um valor: \n");
+        scanf("%d", &valor);
+        if (valor > 0) {
+            soma = soma + valor;
+            numerosPositivos++;
+        }
+    }
+    int media = soma / numerosPositivos;
+    printf("Numeros positivos: %d", numerosPositivos);
+    printf("Media: %d", media);
+}
