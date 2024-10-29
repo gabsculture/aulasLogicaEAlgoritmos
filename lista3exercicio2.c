f2d3a7387303c143b9dbfe14da68d0a6ee810638
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-29 19:53:48 -0300

    Create lista3exercicio2.c

---
+++ lista3exercicio2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int linhas, colunas;
+
+    printf("Digite a quantidade de linhas:");
+    scanf("%d", &linhas);
+    printf("Digite a quantidade de colunas: ");
+    scanf("%d", &colunas);
+    int matrizA[linhas][colunas];
+    int matrizB[linhas][colunas];
+
+    printf("Digite os valores da Matriz A:");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            scanf("%d", &matrizA[i][j]);
+        }
+    }
+    printf("Digite os valores de Matriz B:");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            scanf("%d", &matrizB[i][j]);
+        }
+    }
+
+    int matrizSoma[linhas][colunas];
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            matrizSoma[i][j] = matrizA[i][j] + matrizB[i][j];
+        }
+    }
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matrizSoma[i][j]);
+        }
+        printf("\n");
+    }
+}
