171d0bb5ab16bafac673cedca84f74851a018919
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 22:10:07 -0300

    Update lista2exercicio9.c

--- lista2exercicio9.c
+++ lista2exercicio9.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main() {
+    int p, q, r, s;
+
+    printf("Digite o valor de p, sendo que o valor deve ser par:\n");
+    scanf("%d", &p);
+    if (p % 2 != 0) {
+        printf("O valor não é par, insira novamente");
+        scanf("%d", &p);
+    }
+
+    printf("Digite o valor de q, r e r respectivamente, sendo que o valor deve ser positivo:\n");
+    scanf("%d %d %d", &q, &r, &s);
+    if (q < 0 || r < 0 || s < 0) {
+        printf("O valor não é positivo, insira novamente");
+        scanf("%d %d %d", &q, &r, &s);
+
+
+        int somars = r + s;
+        int somapq = p + q;
+
+        if (q > r && s > p && somars > somapq) {
+            printf("Valores corretos!");
+        } else {
+            printf("Valores errados!");
+        }
+    }
