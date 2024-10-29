2242b11582c9170a16bfcfd312ba257cffa9f915
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 22:32:47 -0300

    Update lista2exercicio13.c

--- lista2exercicio13.c
+++ lista2exercicio13.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int ano;
+    printf("Insira o ano: ");
+    scanf("%d", &ano);
+
+    if(ano % 4 == 0) {
+        printf("O ano eh bissexto!");
+    } else {
+        printf("O ano nao eh bissexto!");
+    }
+}
 
