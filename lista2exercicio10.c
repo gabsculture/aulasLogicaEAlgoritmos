4970ddc009143b139a5e61ae871d714ce77e64a5
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 22:13:31 -0300

    Update lista2exercicio10.c

--- lista2exercicio10.c
+++ lista2exercicio10.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int valor;
+    int soma = 0;
+    for (int i = 1; i <= 5; i++) {
+        printf("Digite um valor: \n");
+        scanf("%d", &valor);
+        if (valor%2 != 0) {
+            soma = soma + valor;
+        }
+    }
+    printf("Soma: %d", soma);
+}
