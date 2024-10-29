9b4b3b3bbb74fda05e00c3e104153ebd8dfee148
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-28 22:15:54 -0300

    Update lista2exercicio11.c

--- lista2exercicio11.c
+++ lista2exercicio11.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int valor;
+    int numerosPositivos = 0;
+    int numerosNegativos = 0;
+    for (int i = 1; i <= 5; i++) {
+        printf("Digite um valor: \n");
+        scanf("%d", &valor);
+        if (valor > 0) {
+            numerosPositivos++;
+        } else {
+            numerosNegativos++;
+        }
+    }
+    printf("Numeros negativos: %d", numerosNegativos);
+    printf("Numeros positivos: %d", numerosPositivos);
+}
 
